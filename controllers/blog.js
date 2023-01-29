e0a0f56cd77763cec73a69e64b1b1c1ad4fe2292
mahak4983 <mahak4983@gmail.com>
2023-01-29 13:27:28 +0530

    add: edit blog route

--- controllers/blog.js
+++ controllers/blog.js
@@ -36,3 +36,41 @@ exports.createBlog = async (req, res) => {
     }
 
 }
+
+exports.editBlog = async (req, res) => {
+    const blogId = req.params.BlogId;
+    const {
+        heading, content
+    } = req.body;
+    try {
+        const blogData = await Blog.findById(blogId,{userId:1});
+        if (!blogData) {
+            return res.status(404).send({ message: "Blog not found" });
+        }
+        if (blogData.userId.toString() !== req.userInfo._id) {
+           return res.status(400).send({ error: "You dont have access to change this blog" });
+        }
+
+        const updatedBlogData = await Blog.updateOne(
+            {
+                _id: blogId,
+            },
+            {
+                $set: {
+                    heading,
+                    content
+                },
+            },
+            
+        );
+
+        res.status(200).send({ message: "successfully edited" });
+
+    } catch (e) {
+        console.log(e);
+        return res.status(500).send({ message: 'Internal server error!' });
+
+    }
+   
+
+}
--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const blogController = require('../controllers/blog');
 const checkAuth = require('../middleware/auth');
 router.post("/create", checkAuth, blogController.createBlog);
+router.post("/edit/:BlogId", checkAuth, blogController.editBlog);
 
 
 module.exports = router;
